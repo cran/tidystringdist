c999744e446ece344e2b0319d6a3c2da3a933a14
Colin Fay <contact@colinfay.me>
2018-11-22 12:40:03 +0000

    version 0.1.3

--- inst/doc/Getting_started.R
+++ inst/doc/Getting_started.R
@@ -5,40 +5,27 @@ knitr::opts_chunk$set(
 )
 
 ## ----eval = FALSE--------------------------------------------------------
-#  devtools::install_github("ColinFay/tidystringdist")
+#  install.packages("tidystringdist")
 
 ## ----eval = FALSE--------------------------------------------------------
-#  install.packages("tidystringdist")
+#  # install.packages(remotes)
+#  remotes::install_github("ColinFay/tidystringdist")
 
 ## ------------------------------------------------------------------------
 library(tidystringdist)
 
 tidy_comb_all(LETTERS[1:3])
 
-## ------------------------------------------------------------------------
 tidy_comb_all(iris, Species)
 
 ## ------------------------------------------------------------------------
-tidy_comb("Paris", state.name[1:3])
+tidy_comb("Paris", state.name)
 
 ## ----example, warnings = FALSE, error=FALSE, message=FALSE---------------
-library(dplyr)
-data(starwars)
-tidy_comb_sw <- tidy_comb_all(starwars, name)
-tidy_stringdist(tidy_comb_sw)
+comb <- tidy_comb_all(state.name) 
+tidy_stringdist(comb)
 
 ## ------------------------------------------------------------------------
-tidy_stringdist(tidy_comb_sw, method = c("osa","jw"))
-
-## ------------------------------------------------------------------------
-tidy_stringdist(tidy_comb_sw, method= "osa") %>%
-  filter(osa > 20) %>%
-  arrange(desc(osa))
-
-## ------------------------------------------------------------------------
-starwars %>%
-  filter(species == "Droid") %>%
-  tidy_comb_all(name) %>%
-  tidy_stringdist() %>% 
-  summarise_if(is.numeric, mean)
+comb <- tidy_comb_all(state.name)
+tidy_stringdist(comb, method = c("osa","jw"))
 
